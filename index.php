f80f1624e86e6b0e786b2cac2448609ec792629c
lokesh <yokesh.ganesan@gmail.com>
2013-04-02 16:44:30 +0530

    Signed-off-by: lokesh <yokesh.ganesan@gmail.com>

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-<?php
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>mmrf</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+  </head>
+  <body>
+   <?php include_once 'header.php';?>
+   
+   
+   
+    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
