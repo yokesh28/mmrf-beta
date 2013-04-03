5a6db59e4450e1f06faa813b48bb65dac155a245
lokesh <yokesh.ganesan@gmail.com>
2013-04-03 12:12:59 +0530

    Signed-off-by: lokesh <yokesh.ganesan@gmail.com>

--- header.php
+++ header.php
@@ -1,1 +1,38 @@
-<?php
+
+<header>
+<div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+      
+          <div class="row-fluid">
+         <a class="span2" href="#"><img src="images/logo.png"></a>
+          <div class="nav-collapse collapse span10 ">
+          
+            <ul class="nav row-fluid ">
+            
+              <li class="span2 ">
+                <a href="#">Home</a>
+              </li>
+              <li class="span2">
+                <a href="#">Projects</a>
+              </li>
+              <li class="span2">
+                <a href="#">About Us</a>
+              </li>
+              <li class="span2">
+                <a href="#">Gallery</a>
+              </li>
+              <li class="span2">
+                <a href="#">Media Center</a>
+              </li>
+              <li class="span2">
+                <a href="#">Contact Us</a>
+              </li>
+             
+            </ul>
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+</header>
--- index.php
+++ index.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+     <link href="css/bootstrap.css" rel="stylesheet" media="screen">
+     <link href="css/bootstrap-responsive.css" rel="stylesheet">
+     <link href="css/style.css" rel="stylesheet">
   </head>
   <body>
    <?php include 'header.php';?>
