6315e0f2bae25f6f5e4f0da0277d79ed7706ae3f
lokesh <yokesh.ganesan@gmail.com>
2013-04-03 17:25:27 +0530

    Signed-off-by: lokesh <yokesh.ganesan@gmail.com>

--- header.php
+++ header.php
@@ -1,45 +1,45 @@
 
 
-	
-	
+
+
 <header>
-<div class="navbar navbar-inverse navbar-fixed-top">
-      <div class="">
-        <div class="container">
-      
-          <div class="row-fluid">
-         <a class="span2" href="#"><img src="images/logo.png"></a>
-          <div class="nav-collapse collapse span10 ">
-          
-	
-            <ul class="nav row-fluid menu">
-            
-              <li class="span2 ">
-                <a href="#">Home</a>
-              </li>
-              <li class="span2">
-                <a href="#">Projects</a>
-              </li>
-              <li class="span2">
-                <a href="#">About Us</a>
-              </li>
-              <li class="span2">
-                <a href="#">Gallery</a>
-              </li>
-              <li class="span2">
-                <a href="#">Media Center</a>
-              </li>
-              <li class="span2">
-                <a href="#">Contact Us</a>
-              </li>
-             
-            </ul>
-          </div>
-        </div>
-        </div>
-      </div>
-    </div>
+	<div class="navbar navbar-inverse navbar-fixed-top">
+		<div class="">
+			<div class="container">
+
+				<div class="row-fluid">
+					<a class="span2 logo" href="#"><img src="images/logo.png"> </a>
+					<div class="nav-collapse collapse span10 ">
+
+
+						<ul class="nav row-fluid menu" id="menu">
+
+							<li class="span2 "><a href="index.html">Home</a>
+							</li>
+							<li class="span2"><a href="projects.html">Projects</a>
+							</li>
+							<li class="span2"><a href="about.html">About Us</a>
+							</li>
+							<li class="span2"><a href="gallery.html">Gallery</a>
+							</li>
+							<li class="span2"><a href="media.html">Media Center</a>
+							</li>
+							<li class="span2"><a href="contact.html">Contact Us</a>
+							</li>
+
+						</ul>
+						<div class="dropmenu"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<script>
+	$(document).ready(function(){
+		$('#menu').mobileMenu({switchWidth :768,prependTo:'.dropmenu',topOptionText :'-MAIN MENU'});
+});
+	</script>
 </header>
 
 
-		
