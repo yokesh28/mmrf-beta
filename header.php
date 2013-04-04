1f1ee041433bf560160a9544ed4f55ff92853be0
DS <DS@ds-9571ec2d157d>
2013-04-04 16:30:07 +0530

    Signed-off-by: DS <DS@ds-9571ec2d157d>

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 
 							<li class="span2 "><a href="index.php">Home</a>
 							</li>
-							<li class="span2"><a href="projects.php">Projects</a>
+							<li class="span2"><a href="project.html">Projects</a>
 							</li>
 							<li class="span2"><a href="about.html">About Us</a>
 							</li>
@@ -34,6 +34,159 @@
 			</div>
 		</div>
 	</div>
+
+
+	<div id="bloc-actus" class="menu-actus" style="display: block;">
+		<div class="actualites-txt">Actualités du Parc national</div>
+		<a class="fleche-noir left show_hide survol-tooltip"
+			style="display: inline;"></a>
+		<div id="fancyNews"
+			style="overflow: hidden; position: relative; width: 400px; height: 244px; display: none;">
+			<a class="fermer show_hide" href="#" style="display: inline;"></a>
+
+			<article
+				class="post-3783 post type-post status-publish format-standard hentry category-actions"
+				id="post-3783">
+				<div title="Date : 28 mars 2013"
+					data-link="/actions/semaine-du-developpement-durable/"
+					style="display: none;">
+					<img alt="Semaine du développement durable"
+						title="Semaine du développement durable" class=""
+						src="img/affiche.jpg"> <span
+						title="Semaine du développement durable">Du 1er au 7 avril 2013,
+						c’est la semaine du développement durable. Même si le
+						développement durable, c’est tous les jours au Parc amazonien de
+						Guyan…</span>
+				</div>
+			</article>
+			<article
+				class="post-3762 post type-post status-publish format-standard hentry category-actions"
+				id="post-3762">
+				<div title="Date : 25 mars 2013"
+					data-link="/actions/le-cedre-sam-cordia-alliodora-une-espece-meconnue-des-guyanais/"
+					style="display: none;">
+					<img
+						alt="Le Cèdre Sam (Cordia alliodora), une espèce méconnue des guyanais !"
+						title="Le Cèdre Sam (Cordia alliodora), une espèce méconnue des guyanais !"
+						class="" src="img/cedre.jpg"> <span
+						title="Le Cèdre Sam (Cordia alliodora), une espèce méconnue des guyanais !">Le
+						Parc amazonien de Guyane, partenaire d’une étude sur le Cèdre Sam
+						(Cordia alliodora) Le Cèdre Sam est un arbre extrêmement exploité
+						par les habitants du bour…</span>
+				</div>
+			</article>
+			<article
+				class="post-3721 post type-post status-publish format-standard hentry category-orpaillage category-vie-des-territoires"
+				id="post-3721">
+				<div title="Date : 12 mars 2013"
+					data-link="/vie-des-territoires/3721/" style="display: none;">
+					<img alt="Manuels Valls à Camopi" title="Manuels Valls à Camopi"
+						class=""
+						src="img/manuel.jpg"> <span
+						title="Manuels Valls à Camopi">Le Ministre de l’intérieur, Manuel
+						Valls, accompagné d’une dizaine de personnes dont le Directeur
+						général de la Gendarmerie nationale, son conseiller gendarmeri…</span>
+				</div>
+			</article>
+			<article
+				class="post-3674 post type-post status-publish format-standard hentry category-actions"
+				id="post-3674">
+				<div title="Date : 21 février 2013"
+					data-link="/actions/souriez-vous-etes-photographie/"
+					style="display: none;">
+					<img alt="Souriez… vous êtes photographié !"
+						title="Souriez… vous êtes photographié !" class=""
+						src="img/puma1oncfs.jpg"> <span
+						title="Souriez… vous êtes photographié !">Alors que nous nous
+						attendons à repérer les hardes de pécaris à lèvres blanches dans
+						le programme du SOPPAG, voici un puma dont l’image a été capturée
+						par…</span>
+				</div>
+			</article>
+			<article
+				class="post-3638 post type-post status-publish format-standard hentry category-gouvernance"
+				id="post-3638">
+				<div title="Date : 08 février 2013"
+					data-link="/gouvernance/rexma-collectif-anti-charte-2-motions-du-bureau-du-conseil-dadministration/"
+					style="display: none;">
+					<img
+						alt="REXMA, collectif anti-charte : 2 motions du bureau du Conseil d’administration"
+						title="REXMA, collectif anti-charte : 2 motions du bureau du Conseil d’administration"
+						class="" src="/assets/motions_bureau_ca_pag-100x100.jpg"> <span
+						title="REXMA, collectif anti-charte : 2 motions du bureau du Conseil d’administration">Le
+						bureau du Conseil d’administration du Parc amazonien de Guyane,
+						réuni en séance le mercredi 06 février 2013, a rédigé deux motions
+						: pour apporter tout son s…</span>
+				</div>
+			</article>
+
+			<div class="fn-newsPreview"
+				style="width: 400px; height: 120px; background-color: rgba(0, 0, 0, 0.701961); top: 0px; display: block;">
+				<img class="fn-newsPreviewThumb"
+					src="/assets/affiche_sdd-100x100.jpg"><span
+					class="fn-newsPreviewText"><h3>Semaine du développement durable</h3>
+					<h4>Date : 28 mars 2013</h4>Du 1er au 7 avril 2013, c’est la
+					semaine du développement durable. Même si le développement durable,
+					c’est tous les jours au Parc amazonien de Guyan…[....]</span>
+			</div>
+			<div class="fn-newsPreview"
+				style="width: 400px; height: 120px; background-color: rgba(0, 0, 0, 0.701961); top: 122px; display: block;">
+				<img class="fn-newsPreviewThumb"
+					src="/assets/cedre-sam2-100x100.jpg"><span
+					class="fn-newsPreviewText"><h3>Le Cèdre Sam (Cordia alliodora), une
+						espèce méconnue des guyanais !</h3>
+					<h4>Date : 25 mars 2013</h4>Le Parc amazonien de Guyane, partenaire
+					d’une étude sur le Cèdre Sam (Cordia alliodora) Le Cèdre Sam est un
+					arbre extrêmement exploité par les habitants du bour…[....]</span>
+			</div>
+			<div class="fn-newsPreview"
+				style="width: 400px; height: 120px; background-color: rgba(0, 0, 0, 0.701961); top: 244px; background-position: initial initial; background-repeat: initial initial;">
+				<img class="fn-newsPreviewThumb"
+					src="/assets/manuel-valls-a-camopipag-jmmontoute-100x100.jpg"><span
+					class="fn-newsPreviewText"><h3>Manuels Valls à Camopi</h3>
+					<h4>Date : 12 mars 2013</h4>Le Ministre de l’intérieur, Manuel
+					Valls, accompagné d’une dizaine de personnes dont le Directeur
+					général de la Gendarmerie nationale, son conseiller
+					gendarmeri…[....]</span>
+			</div>
+			<div class="fn-newsPreview"
+				style="width: 400px; height: 120px; background-color: rgba(0, 0, 0, 0.701961); top: 366px; background-position: initial initial; background-repeat: initial initial;">
+				<img class="fn-newsPreviewThumb"
+					src="/assets/puma1oncfs-pag-100x100.jpg"><span
+					class="fn-newsPreviewText"><h3>Souriez… vous êtes photographié !</h3>
+					<h4>Date : 21 février 2013</h4>Alors que nous nous attendons à
+					repérer les hardes de pécaris à lèvres blanches dans le programme
+					du SOPPAG, voici un puma dont l’image a été capturée par…[....]</span>
+			</div>
+			<div class="fn-newsPreview"
+				style="width: 400px; height: 120px; background-color: rgba(0, 0, 0, 0.701961); top: 488px; background-position: initial initial; background-repeat: initial initial;">
+				<img class="fn-newsPreviewThumb"
+					src="/assets/motions_bureau_ca_pag-100x100.jpg"><span
+					class="fn-newsPreviewText"><h3>REXMA, collectif anti-charte : 2
+						motions du bureau du Conseil d’administration</h3>
+					<h4>Date : 08 février 2013</h4>Le bureau du Conseil
+					d’administration du Parc amazonien de Guyane, réuni en séance le
+					mercredi 06 février 2013, a rédigé deux motions : pour apporter
+					tout son s…[....]</span>
+			</div>
+			<div id="fn-newsView" style="width: 400px; height: 244px;">
+				<h3 id="fn-newsViewTitle"></h3>
+				<div id="fn-newsViewHtml"
+					style="height: 194px; overflow: hidden; padding: 0px; width: 0px;">
+					<div class="jspContainer" style="width: 0px; height: 194px;">
+						<div class="jspPane" style="padding: 0px; top: 0px; width: 0px;"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="fn-newsFooterBar"
+			style="width: 400px; background-color: rgb(0, 0, 0); display: none;">
+			<span id="newsFooterText"></span><span id="fn-previousButton"></span><span
+				id="fn-nextButton"></span>
+			<div id="fn-newsViewClose" style="display: none;"></div>
+		</div>
+	</div>
+
 
 
 	<script>
