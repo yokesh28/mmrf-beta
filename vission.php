48d9e3cd741451481ff04b933c58512d347b1dab
janani <janu.vu@gmail.com>
2013-04-08 18:52:09 +0530

    Signed-off-by: janani <janu.vu@gmail.com>

--- vission.php
+++ vission.php
@@ -1,122 +1,136 @@
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 <meta charset="UTF-8" />
 
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+<script
+	type="text/javascript"
+	src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
 
 
 
-<link href="css/style.css" rel="stylesheet">
-<link href="css/res.css" rel="stylesheet">
-<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
-<link href="css/bootstrap.css" rel="stylesheet" media="screen">
-<link  href="css/bootstrap-responsive.css" rel="stylesheet">
-<link href="css/style.css" rel="stylesheet">
+<link href="css/style.css"
+	rel="stylesheet">
+<link href="css/res.css"
+	rel="stylesheet">
+<link
+	href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+<link
+	href="css/bootstrap.css" rel="stylesheet" media="screen">
+<link
+	href="css/bootstrap-responsive.css" rel="stylesheet">
+<link href="css/style.css"
+	rel="stylesheet">
 <script src="js/bootstrap.min.js"></script>
 
 
-<script src="js/jquery.mobilemenu.js"></script>
+<script
+	src="js/jquery.mobilemenu.js"></script>
 
 
 
 <?php include 'header.php';?>
 <div class="row-fluid">
- <div class="vission">
-	<div class="span3">
-		<ul class="unstyled menu1">
-			<li class="color">About Us</li>
-			<li>Management</li>
-			<li>Values</li>
-			<li>Certification</li>
+	<div class="vission">
+		<div class="span3">
+			<ul class="unstyled menu1 span10">
+
+				<li><img src="images/group.png" class="img-rounded"><p> About Us</p>
+				</li>
+				<li><img src="images/management.png" class="img-rounded"><p>Management</p>
+				</li>
+				<li><img src="images/team.png" class="img-rounded"><p>Team</p>
+				</li>
 
 
-		</ul>
-	</div>
-	<div class="row  span9 align">
-		<div class="span8">
-			<div class="span2">
-				<h3>Vission</h3>
 
-			</div>
-			<div class="span10">
-				<p>To be the language that real estate speaks. This vision has
-					inspired us towards making each space, a standard of fine living.
-					Every space we build should mark a new milestone in the entire
-					industry. Every creation by us, should exhibit technological
-					advancement and design efficiency. We want to exist in every
-					thought you have, of building a new space. We want our creations to
-					facilitate the development of a better society. Sustainability of a
-					holistic environment is what we will continually strive to achieve.
-					Our creations evolve to define a quality lifestyle. Every element
-					of the space we design will accentuate functional significance. We
-					aim to provide an experience of an entirely new world which
-					encompasses differences in cultures, professions and value systems.</p>
-			</div>
+			</ul>
 		</div>
-		<div class="span8">
-			<div class="span2">
-				<h3>Mission</h3>
+		<div class="row  span9 align">
+			<div class="span8">
+				<div class="span2">
+					<h3>Vission</h3>
 
+				</div>
+				<div class="span10">
+					<p>To be the language that real estate speaks. This vision has
+						inspired us towards making each space, a standard of fine living.
+						Every space we build should mark a new milestone in the entire
+						industry. Every creation by us, should exhibit technological
+						advancement and design efficiency. We want to exist in every
+						thought you have, of building a new space. We want our creations
+						to facilitate the development of a better society. Sustainability
+						of a holistic environment is what we will continually strive to
+						achieve. Our creations evolve to define a quality lifestyle. Every
+						element of the space we design will accentuate functional
+						significance. We aim to provide an experience of an entirely new
+						world which encompasses differences in cultures, professions and
+						value systems.</p>
+				</div>
 			</div>
-			<div class="span10">
+			<div class="span8">
+				<div class="span2">
+					<h3>Mission</h3>
 
-				<p>To build spaces, that are constructive to societal welfare. To
-					facilitate convenience in lifestyle. To enable transparency in
-					service and communication. To provide world class infrastructure.
-					To promote a stable, happy and healthy environment. To be the most
-					credible brand in the real estate industry. To inculcate a
-					cosmopolitan society.</p>
+				</div>
+				<div class="span10">
 
+					<p>To build spaces, that are constructive to societal welfare. To
+						facilitate convenience in lifestyle. To enable transparency in
+						service and communication. To provide world class infrastructure.
+						To promote a stable, happy and healthy environment. To be the most
+						credible brand in the real estate industry. To inculcate a
+						cosmopolitan society.</p>
+
+				</div>
 			</div>
+
+
+			<div class="span8">
+				<div class="span2">
+					<h3>Customer Satisfaction</h3>
+
+				</div>
+				<div class="span10">
+
+					<p>At Royal Splendour Developers, we eternally look to serve as
+						architects of your dream. Every space we build, is metaphorical to
+						the joy and comfort you would like to indulge in. We enable you to
+						realize your aspirations in actuality. Each space that we build is
+						only a physical embodiment of our credibility. Every project marks
+						the growth of our credibility. We form ideal environments and
+						expose you to a society you would like to live in. Through our
+						architectural splendors, we have established our adherence to our
+						social and environmental responsibility. We only hope that our
+						sensitivity for your needs, translates into your happiness and an
+						increased standard of living.</p>
+
+				</div>
+			</div>
+
+
+			<div class="span8">
+				<div class="span2">
+					<h3>Quality And Delivery</h3>
+
+				</div>
+				<div class="span10">
+
+					<p>We look to build, what is symbolic to a world-class lifestyle.
+						The functional efficiency of every home we build, perfectly
+						juxtaposes the aesthetics it exhibits. Each space we build,
+						displays exclusivity through our well planned and executed
+						designs. Whether it is amenities as basic as parking facilities or
+						as essential as security, the infrastructure we strive to provide
+						you with, does not compromise with the standards that we ensure.
+						The Finesse that you see, is a reflection of the high quality of
+						supplies that we use and the technically advanced system of
+						execution that we follow. We look to strike a perfect balance
+						between the structural quality of what we build and the facilities
+						we have to offer you.</p>
+
+				</div>
+			</div>
+
+
 		</div>
-
-
-		<div class="span8">
-			<div class="span2">
-				<h3>Customer Satisfaction</h3>
-
-			</div>
-			<div class="span10">
-
-				<p>At Royal Splendour Developers, we eternally look to serve as
-					architects of your dream. Every space we build, is metaphorical to
-					the joy and comfort you would like to indulge in. We enable you to
-					realize your aspirations in actuality. Each space that we build is
-					only a physical embodiment of our credibility. Every project marks
-					the growth of our credibility. We form ideal environments and
-					expose you to a society you would like to live in. Through our
-					architectural splendors, we have established our adherence to our
-					social and environmental responsibility. We only hope that our
-					sensitivity for your needs, translates into your happiness and an
-					increased standard of living.</p>
-
-			</div>
-		</div>
-
-
-		<div class="span8">
-			<div class="span2">
-				<h3>Quality And Delivery</h3>
-
-			</div>
-			<div class="span10">
-
-				<p>We look to build, what is symbolic to a world-class lifestyle.
-					The functional efficiency of every home we build, perfectly
-					juxtaposes the aesthetics it exhibits. Each space we build,
-					displays exclusivity through our well planned and executed designs.
-					Whether it is amenities as basic as parking facilities or as
-					essential as security, the infrastructure we strive to provide you
-					with, does not compromise with the standards that we ensure. The
-					Finesse that you see, is a reflection of the high quality of
-					supplies that we use and the technically advanced system of
-					execution that we follow. We look to strike a perfect balance
-					between the structural quality of what we build and the facilities
-					we have to offer you.</p>
-
-			</div>
-		</div>
-
-
-	</div>
 	</div>
 </div>
