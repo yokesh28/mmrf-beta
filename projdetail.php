022868ad6d488d007c8bd177dc3cf3d202a97af2
lokesh <yokesh.ganesan@gmail.com>
2013-04-08 17:29:13 +0530

    Signed-off-by: lokesh <yokesh.ganesan@gmail.com>

--- projdetail.php
+++ projdetail.php
@@ -3,7 +3,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
->
+
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
@@ -262,7 +262,9 @@
 					<!-- / uc-container -->
 
 				</div>
-				<!-- / grid --> </section>
+				
+				<!-- / grid -->
+				 </section>
 
 			</div>
 		</div>
