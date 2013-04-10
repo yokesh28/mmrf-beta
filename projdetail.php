ec12c2b4bf32a694da6e061665f084166a38448e
janani <janu.vu@gmail.com>
2013-04-10 18:22:53 +0530

    Signed-off-by: janani <janu.vu@gmail.com>

--- projdetail.php
+++ projdetail.php
@@ -84,9 +84,9 @@
 				<div id="grid" class="grid clearfix span12">
 
 
-					<div class="uc-container box1 span4">
+					<div class="uc-container box1 span4 overview">
 						<div class="uc-initial-content">
-							<p>Overview</p>
+							
 							<span class="icon-eye">Overview</span>
 
 						</div>
@@ -200,9 +200,9 @@
 
 
 
-					<div class="uc-container box2 span4">
+					<div class="uc-container box2 span4 location">
 						<div class="uc-initial-content">
-							<p>Location</p>
+							
 							<span class="icon-eye"></span>
 						</div>
 						<div class="uc-final-content ">
@@ -221,9 +221,9 @@
 					</div>
 					<!-- / uc-container -->
 
-					<div class="uc-container box3 span4">
+					<div class="uc-container box3 span4 elevation">
 						<div class="uc-initial-content">
-							<p>Elevation</p>
+							
 							<span class="icon-eye"></span>
 						</div>
 						<div class="uc-final-content ">
@@ -233,9 +233,9 @@
 					</div>
 					<!-- / uc-container -->
 
-					<div class="uc-container box4 span4">
+					<div class="uc-container box4 span4 amenities">
 						<div class="uc-initial-content">
-							<p>Amenities</p>
+							
 							<span class="icon-eye"></span>
 						</div>
 						<div class="uc-final-content">
@@ -267,9 +267,9 @@
 					</div>
 
 
-					<div class="uc-container box5 span4">
+					<div class="uc-container box5 span4 specification">
 						<div class="uc-initial-content">
-							<p>Specifications</p>
+							
 							<span class="icon-eye"></span>
 						</div>
 						<div class="uc-final-content">
@@ -284,15 +284,15 @@
 								</p>
 								<br>
 								<p>
-<<<<<<< HEAD
+
 									<b>Doors:</b>MainDoor:Flush doors with Wooden Frame<br> <b>InternalDoor:</b>Flush
 									doors with Wooden Frame<br> <b>ToiletDoor:</b>Water Proof Flush
 									Door
-=======
+
 									<b>Doors:</b>MainDoor:Flush doors with Wooden Frame<br><br>
 									<b>InternalDoor:</b>Flush doors with Wooden Frame<br><br><b>ToiletDoor:</b>Water
 									Proof Flush Door
->>>>>>> branch 'master' of https://github.com/yokesh28/mmrf-beta.git
+
 								</p>
 								<br>
 								<p>
@@ -360,10 +360,10 @@
 
 
 
-					<div class="uc-container box6 span4">
+					<div class="uc-container box6 span4 floorplan">
 
 						<div class="uc-initial-content">
-							<p>Floor Plans</p>
+							
 							<span class="icon-eye"></span>
 						</div>
 						<div class="uc-final-content">
