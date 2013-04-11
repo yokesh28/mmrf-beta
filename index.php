156921980c0b911d6d33e55d18a7ea96d747cb4c
lokesh <yokesh.ganesan@gmail.com>
2013-04-11 18:03:13 +0530

    Signed-off-by: lokesh <yokesh.ganesan@gmail.com>

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
 
 <meta charset="utf-8">
-<title>Parc Amazonien de Guyane</title>
+<title>mmrf</title>
 <meta name="viewport"
 	content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
 <link rel='shortcut icon' href='/img/favicon.png'>
@@ -22,17 +22,6 @@
 <!-- / Yoast WordPress SEO plugin. -->
 
 
-<script type="text/javascript">
-		var _gaq = _gaq || [];
-	 	_gaq.push(['_setAccount', 'UA-33459997-1']);
-	 	_gaq.push(['_trackPageview']);
-
-		(function() {
-			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	 	})();
-	</script>
 
 
 
