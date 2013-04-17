979ea8e2164755bb5ed3043ee400abea2781ef56
lokesh <yokesh.ganesan@gmail.com>
2013-04-17 18:43:44 +0530

    Signed-off-by: lokesh <yokesh.ganesan@gmail.com>

--- about.php
+++ about.php
@@ -7,8 +7,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 
-<meta name="keywords"
-	content="" />
+<meta name="keywords" content="" />
 
 
 <link rel="stylesheet" type="text/css" href="css/demo.css" />
@@ -40,34 +39,21 @@
 
 <body id="about">
 
-<img src="images/2.jpg" width="100%" height="100%"  class="backimage back">
+	<img src="images/2.jpg" width="100%" height="100%"
+		class="backimage back">
 
 	<?php include 'header.php';?>
 	<div class="container">
 		<div class="row-fluid ">
 			<div class="vission">
-				<div class="span3 down">
 
-					<ul class="unstyled menu1 span10 effect1" id="myTab">
-						<li class="active"><a href="#aboutcontent"><img
-								src="images/group.png" class="img-rounded">
-								<p>About Us</p> </a>
-						</li>
-						<li><a href="#management"> <img src="images/management.png"
-								class="img-rounded">
-								<p>Management</p>
-						</a>
-						</li>
-						<li><a href="#app"><img src="images/team.png" class="img-rounded">
-								<p>Team</p> </a>
-						</li>
-					</ul>
-
-				</div>
 
 				<div class="tab-content span9">
 					<div class="tab-pane active overview effect1" id="aboutcontent">
-						<h4>Overview</h4>
+						<h4 class="">
+							<img src="images/group.png" class="img-rounded"><span
+								class="txt_under_headin"> OVERVIEW</span>
+						</h4>
 						<p class="">The top-notch names of the Real Estate sector have
 							joined hands to create and bestow an exceptional projects with
 							unparalleled quality. MMRF is a collaboraive endeavor of four
@@ -87,97 +73,102 @@
 
 
 					<div class="tab-pane effect1" id="management">
+						<h4 class="">
+							<img src="images/management.png" class="img-rounded"><span
+								class="txt_under_headin"> MANAGEMENT</span>
+						</h4>
+						<div class="mange_over">
+							<div class="span12"></div>
+							<div class="content span12">
+								<img src="img/about.png">
+								<h4>Mr.V.Mohan</h4>
 
-						<div class="span12"></div>
-						<div class="content span12">
-							<img src="img/about.png">
-							<h4>Mr.V.Mohan</h4>
+								<h5>
+									<span>(Managing Director of<a
+										href="http://www.mayflowerenterprises.com/" target="_blank"><br>
+											Mayflower Enterprises Pvt.Ltd)</a>
+									</span>
+								</h5>
 
-							<h5>
-								<span>(Managing Director of<a
-									href="http://www.mayflowerenterprises.com/" target="_blank"><br>
-										Mayflower Enterprises Pvt.Ltd)</a>
-								</span>
-							</h5>
+								<p class="span7">Mayflower was established in 1985 as property
+									developers in Coimbatore by Mr. A.N. Venkatachalam, one of the
+									key members of the Chettinad business empire for over 5
+									decades. Under the leadership of Mr. V Mohan, the Managing
+									Director, the company has today grown to become a leading
+									developer of gated communities, independent villas and
+									townships. The company has completed over 8 commercial projects
+									measuring over 250,000 sq. ft and 18 residential projects –
+									apartment complexes and independent villas measuring an
+									estimated 1,200,000 sq. ft.</p>
+								<br>
+							</div>
 
-							<p class="span7">Mayflower was established in 1985 as property
-								developers in Coimbatore by Mr. A.N. Venkatachalam, one of the
-								key members of the Chettinad business empire for over 5 decades.
-								Under the leadership of Mr. V Mohan, the Managing Director, the
-								company has today grown to become a leading developer of gated
-								communities, independent villas and townships. The company has
-								completed over 8 commercial projects measuring over 250,000 sq.
-								ft and 18 residential projects – apartment complexes and
-								independent villas measuring an estimated 1,200,000 sq. ft.</p>
-							<br>
+
+
+							<div class="content span12">
+								<img src="img/about.png">
+								<h4>Mr. Ramesh Bafna</h4>
+								<h5>
+									<span>(Managing Director of<a
+										href="http://www.mayflowerenterprises.com/" target="_blank"><br>
+											Mount Housing &amp; Infrastructure Ltd)</a>
+									</span>
+								</h5>
+								<p class="span7">Mount Housing &amp; Infrastructure Limited is
+									one of the leading developers in Coimbatore in the areas of
+									residential flats and commercial buildings. The company has
+									executed over 500,000 sq. ft of constructions across 7 projects
+									and has an estimated 325,000 sq. ft of construction currently
+									underway.</p>
+							</div>
+
+							<div class="content span12">
+								<img src="img/about.png">
+								<h4>Mr. V. Subramanian</h4>
+								<h5>
+									<span>(Managing Director of<a
+										href="http://www.mayflowerenterprises.com/" target="_blank"><br>
+											Ramani Realtors Pvt. Limited)</a>
+									</span>
+								</h5>
+								<p class="span7">
+									Ramani Realtors Pvt. Limited – RRPL, an ISO 9001:2008 certified
+									Company is professionally managed by its Chairman Shri V S
+									Venkatraman Founded as Rajmani Builders, VSV Builders &amp;
+									Balaji Constructions by its Chairman, the Company has projects
+									across Chennai and Coimbatore Mr V Subramanian joined his
+									Father Shri V S Venkatraman in 1981 and established Ramani
+									Realtors in the year 1987. He is the Managing Director of the
+									Company and keeps himself abreast of the latest developments in
+									the construction industry and implements them in his projects.
+									He is ably supported by its Executive Director Mrs Dharma
+									Subramanian. RRPL is one of the volume leaders for budget homes
+									in Chennai &amp; Coimbatore with over 4000 satisfied Customer
+									families and over 4 million sq ft of completed constructions.<br>
+								</p>
+							</div>
+							<div class="content span12">
+								<img src="img/about.png">
+								<h4>Mr. Saravanan</h4>
+								<h5>
+									<span>(Managing Director of<a
+										href="http://www.mayflowerenterprises.com/" target="_blank"><br>
+											Fairyland Foundations Pvt. Ltd)</a>
+									</span>
+								</h5>
+								<p class="span7">Established in 2000, Fairyland Foundations Pvt.
+									Ltd. is a one of the leading builders in Chennai and is engaged
+									in the construction of residential flats and individual homes
+									in Chennai. Spurred by its remarkable success in Chennai, the
+									Company has now spread its wings to Hosur and Coimbatore which
+									have been witnessing rapid and consistent growth in recent
+									years. The company has completed over 14 residential projects
+									comprising of 500 flats and 40 individual &amp; duplex units
+									spanning across 650,000sq. ft.</p>
+
+							</div>
+
 						</div>
-
-
-
-						<div class="content span12">
-							<img src="img/about.png">
-							<h4>Mr. Ramesh Bafna</h4>
-							<h5>
-								<span>(Managing Director of<a
-									href="http://www.mayflowerenterprises.com/" target="_blank"><br>
-										Mount Housing &amp; Infrastructure Ltd)</a>
-								</span>
-							</h5>
-							<p class="span7">Mount Housing &amp; Infrastructure Limited is
-								one of the leading developers in Coimbatore in the areas of
-								residential flats and commercial buildings. The company has
-								executed over 500,000 sq. ft of constructions across 7 projects
-								and has an estimated 325,000 sq. ft of construction currently
-								underway.</p>
-						</div>
-
-						<div class="content span12">
-							<img src="img/about.png">
-							<h4>Mr. V. Subramanian</h4>
-							<h5>
-								<span>(Managing Director of<a
-									href="http://www.mayflowerenterprises.com/" target="_blank"><br>
-										Ramani Realtors Pvt. Limited)</a>
-								</span>
-							</h5>
-							<p class="span7">
-								Ramani Realtors Pvt. Limited – RRPL, an ISO 9001:2008 certified
-								Company is professionally managed by its Chairman Shri V S
-								Venkatraman Founded as Rajmani Builders, VSV Builders &amp;
-								Balaji Constructions by its Chairman, the Company has projects
-								across Chennai and Coimbatore Mr V Subramanian joined his Father
-								Shri V S Venkatraman in 1981 and established Ramani Realtors in
-								the year 1987. He is the Managing Director of the Company and
-								keeps himself abreast of the latest developments in the
-								construction industry and implements them in his projects. He is
-								ably supported by its Executive Director Mrs Dharma Subramanian.
-								RRPL is one of the volume leaders for budget homes in Chennai
-								&amp; Coimbatore with over 4000 satisfied Customer families and
-								over 4 million sq ft of completed constructions.<br>
-							</p>
-						</div>
-						<div class="content span12">
-							<img src="img/about.png">
-							<h4>Mr. Saravanan</h4>
-							<h5>
-								<span>(Managing Director of<a
-									href="http://www.mayflowerenterprises.com/" target="_blank"><br>
-										Fairyland Foundations Pvt. Ltd)</a>
-								</span>
-							</h5>
-							<p class="span7">Established in 2000, Fairyland Foundations Pvt.
-								Ltd. is a one of the leading builders in Chennai and is engaged
-								in the construction of residential flats and individual homes in
-								Chennai. Spurred by its remarkable success in Chennai, the
-								Company has now spread its wings to Hosur and Coimbatore which
-								have been witnessing rapid and consistent growth in recent
-								years. The company has completed over 14 residential projects
-								comprising of 500 flats and 40 individual &amp; duplex units
-								spanning across 650,000sq. ft.</p>
-
-						</div>
-
-
 					</div>
 
 
@@ -185,12 +176,11 @@
 
 					<div class="tab-pane effect1" id="app">
 
-
+						<h4 class="">
+							<img src="images/management.png" class="img-rounded"><span
+								class="txt_under_headin"> TEAM</span>
+						</h4>
 						<div class="bd" id="content">
-
-							<blockquote>
-								<p>Meet the team behind the name.</p>
-							</blockquote>
 							<div class="timeline">
 								<ol class="hcard clearfix">
 									<li>
@@ -255,6 +245,24 @@
 			</div>
 		</div>
 	</div>
+	<div class="span3 down">
+
+		<ul class="unstyled menu1" id="myTab">
+			<li class="active"><a href="#aboutcontent"><img
+					src="images/group.png" class="img-rounded">
+					<p class="menu_text txt_under">Overview</p> </a>
+			</li>
+			<li><a href="#management"> <img src="images/management.png"
+					class="img-rounded">
+					<p class="menu_text txt_under">Management</p>
+			</a>
+			</li>
+			<li><a href="#app"><img src="images/team.png" class="img-rounded">
+					<p class="menu_text txt_under">Team</p> </a>
+			</li>
+		</ul>
+
+	</div>
 	<?php include 'footer.php';?>
 </body>
 <script>
@@ -264,6 +272,29 @@
   e.preventDefault();
   $(this).tab('show');
 })
+  
+  
+ $(".menu1 li").mouseenter(function(){
+	 $(this).clearQueue();
+	 $(this).find('.txt_under').animate({'background-position': '-50px'},500);
+	 });
+  $(".menu1 li").mouseleave(function(){
+	  $(this).clearQueue();
+		 $(this).find('.txt_under').animate({'background-position': '150px'},500);
+		 });
+
+  $(window).load(function() {
+	  $(".down").delay(1500).animate({right:'-173px'},700);
+  });
+  
+  $(".down").mouseenter(function(){
+	  $(".down").animate({right:'-20px'},700);
+  });
+  
+  $(".down").mouseleave(function(){
+	  $(this).clearQueue();
+	  $(".down").delay(500).animate({right:'-173px'},700);
+  });
 </script>
 <script src="js/bootstrap-tab.js"></script>
 </html>
