41eb56c03ea263579e943e43cd877b45e4302aa4
janani <janu.vu@gmail.com>
2013-04-23 11:05:03 +0530

    Signed-off-by: janani <janu.vu@gmail.com>

--- vistaoceana/js/lightbox.js
+++ vistaoceana/js/lightbox.js
@@ -49,7 +49,7 @@ lightbox = new Lightbox options
 
     function LightboxOptions() {
       this.fileLoadingImage = 'images/loading.gif';
-      this.fileCloseImage = 'images/close.png';
+      this.fileCloseImage = 'menulist/cls.png';
       this.resizeDuration = 700;
       this.fadeDuration = 500;
       this.labelImage = "Image";
