103f21d0664e42ca78da73c7cbd1bf3ffa225065
DS <DS@ds-9571ec2d157d>
2013-04-30 17:47:31 +0530

    Signed-off-by: DS <DS@ds-9571ec2d157d>

--- media.php
+++ media.php
@@ -72,9 +72,9 @@
 			<div class="span3">
 				<a class="fancybox" href="saienclave.pdf" target="_blank"> <img
 					src="img/pdf1.png" class="img-polaroid">
-				</a> <a class="fancybox" href="saienclave.pdf">
+				</a> 
 					<h5>SaiEnclave(10/4/2013)</h5>
-				</a>
+				
 
 			</div>
 
