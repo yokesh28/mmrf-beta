e018dadc97669461dbc560ac2fe29ce035f6788a
Lokesh <yokesh.ganesan@gmail.com>
2013-05-06 11:07:21 +0530

    Signed-off-by: Lokesh <yokesh.ganesan@gmail.com>

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
 
 
 
-<script src="js/jsscript.js"></script>
+
 <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
 <!--[if lt IE 9]>
   <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
